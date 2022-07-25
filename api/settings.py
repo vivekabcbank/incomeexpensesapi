a3b20d285504042f2201e6f9649985d8293d38f8
vivekabcbank <vivek.athilkar@bank-abc.com>
2022-07-25 20:22:18 +0530

    paginations

--- api/settings.py
+++ api/settings.py
@@ -135,6 +135,9 @@ SWAGGER_SETTINGS = {
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
     "NON_FIELD_ERRORS_KEY":"error",
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
